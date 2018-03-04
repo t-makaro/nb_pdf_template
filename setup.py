a3fb5969726945c02297b60203f5b65711435666
Tyler Makaro <tyler@makaro.com>
2018-03-04 09:26:42 -0800

    release 1.1.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='nb_pdf_template',
-      version='1.1.1',
+      version='1.1.2',
       description='LaTeX templates for jupyter notebook conversion to pdf',
       url='https://github.com/t-makaro/nb_pdf_template',
       author='Tyler Makaro',
